347c3865a7113bf95b3df0380ab6b3297249c9b0
Adam Beeson <adambeeson@OR-TO-12118.local>
2024-04-10 19:59:45 -0600

    moving files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { trace } from '@opentelemetry/api';
 import express, { Express } from 'express';
-import { DiceRollingService } from '../dice';
+import { DiceRollingService } from './dice';
 
 const PORT: number = parseInt(process.env.PORT || '8080');
 const traceName: string = process.env.TRACE_NAME || 'dice-server';
@@ -10,10 +10,10 @@ const diceRollingService = new DiceRollingService();
 const app: Express = express();
 
 app.get('/rolldice', (req, res) => {
-    const randomNumber = diceRollingService.rollTheDice(3, 1, 6);
-    res.send(randomNumber.toString());
+  const randomNumber = diceRollingService.rollTheDice(3, 1, 6);
+  res.send(randomNumber.toString());
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening for requests on http://localhost:${PORT}`);
+  console.log(`Listening for requests on http://localhost:${PORT}`);
 });
